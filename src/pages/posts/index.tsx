6bc53f15777411eb8d34f566892131f5fb753075
Vinícius Pereira de Oliveira <vipoliveira2012@live.com>
2022-03-07 22:54:05 -0300

    feat: Página detalhes do post e sobre

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -10,47 +10,55 @@ import Image from 'next/image';
 
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
-import { RichText } from 'prismic-dom';
+import { RichText} from 'prismic-dom'; 
 
 // https://png-pixel.com/
 
-import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi';
 
 type Post = {
-  slug: string,
-  title: string,
-  cover: string,
-  description: string,
+  slug: string;
+  title: string;
+  cover: string;
+  description: string;
   updatedAt: string
 }
 
-interface PostsProps {
-  posts: Post[],
-  page: string,
-  totalPage: string
+interface PostsProps{
+  posts: Post[];
+  page: string;
+  totalPage: string;
 }
 
-export default function Posts({ posts: postsBlog, page, totalPage }: PostsProps) {
-  const [currentPage, setCurrentPage] = useState(Number(page))
+export default function Posts({ posts: postsBlog, page, totalPage }: PostsProps){
+
+  const [currentPage, setCurrentPage] = useState(Number(page)); 
   const [posts, setPosts] = useState(postsBlog || []);
 
-  async function reqPost(pageNumber: number) {
-    const prismic = getPrismicClient()
+  //Buscar novos posts
+  async function reqPost(pageNumber: number){
+    const prismic = getPrismicClient();
+
     const response = await prismic.query([
       Prismic.Predicates.at('document.type', 'post')
     ], {
       orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
       fetch: ['post.title', 'post.description', 'post.cover'],
-      pageSize: 2,
-      page: String(pageNumber)
+      pageSize: 3,
+      page: String(pageNumber)  
     })
-    return response
-  }
-  async function navigatePage(pageNumber: number) {
-    const response = await reqPost(pageNumber)
-    if (response.results.length === 0) return;
 
-    const getPosts = response.results.map(post => {
+    return response;
+  }
+
+  async function navigatePage(pageNumber: number){
+    const response = await reqPost(pageNumber);
+
+    if(response.results.length === 0){
+    return;      
+    }
+
+    const getPosts = response.results.map( post => {
       return {
         slug: post.uid,
         title: RichText.asText(post.data.title),
@@ -63,60 +71,64 @@ export default function Posts({ posts: postsBlog, page, totalPage }: PostsProps)
         })
       }
     })
+
     setCurrentPage(pageNumber)
-    setPosts(getPosts)
+    setPosts(getPosts);
+
   }
 
-  return (
+  return(
     <>
-      <Head>
-        <title>Blog | Sujeito Programador</title>
-      </Head>
-      <main className={styles.container}>
-        <div className={styles.posts}>
-          {posts.map(post => (
-            <Link key={post.slug} href={`/posts/${post.slug}`}>
-              <a key={post.slug}>
-                <Image
-                  src={post.cover}
-                  alt={post.title}
-                  width={720}
-                  height={410}
-                  quality={100}
-                  placeholder="blur"
-                  blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
-                />
-                <strong>{post.title}</strong>
-                <time>{post.updatedAt}</time>
-                <p>{post.description}</p>
-              </a>
-            </Link>
-          ))}
-          <div className={styles.buttonNavigate}>
-            {Number(currentPage) >= 2 && (
+     <Head>
+       <title>Blog | Sujeito Programador</title>
+     </Head>
+     <main className={styles.container}>
+       <div className={styles.posts}>
+        {posts.map( post => (
+          <Link key={post.slug} href={`/posts/${post.slug}`}>
+            <a key={post.slug}>
+              <Image 
+                src={post.cover} 
+                alt={post.title}
+                width={720}
+                height={410}
+                quality={100}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
+              />
+              <strong>{post.title}</strong>
+              <time>{post.updatedAt}</time>
+              <p>{post.description}</p>
+            </a>
+          </Link>
+        ))}
+
+         <div className={styles.buttonNavigate}>
+            { Number(currentPage) >= 2 && (
               <div>
-                <button onClick={() => navigatePage(1)}>
+                <button onClick={ () => navigatePage(1) }>
                   <FiChevronsLeft size={25} color="#FFF" />
                 </button>
-                <button onClick={() => navigatePage(Number(currentPage - 1))}>
+                <button onClick={ () => navigatePage(Number(currentPage - 1)) }>
                   <FiChevronLeft size={25} color="#FFF" />
                 </button>
               </div>
             )}
 
-            {Number(currentPage) < Number(totalPage) && (
+            { Number(currentPage) < Number(totalPage) && (
               <div>
-                <button onClick={() => navigatePage(Number(currentPage + 1))}>
+                <button onClick={ () => navigatePage(Number(currentPage + 1)) }>
                   <FiChevronRight size={25} color="#FFF" />
                 </button>
-                <button onClick={() => navigatePage(Number(totalPage))}>
+                <button onClick={ () => navigatePage(Number(totalPage)) }>
                   <FiChevronsRight size={25} color="#FFF" />
                 </button>
               </div>
             )}
-          </div>
-        </div>
-      </main>
+            
+         </div>
+       </div>
+     </main>
     </>
   )
 }
@@ -130,12 +142,12 @@ export const getStaticProps: GetStaticProps = async () => {
   ], {
     orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
     fetch: ['post.title', 'post.description', 'post.cover'],
-    pageSize: 2
+    pageSize: 3
   })
 
   // console.log(JSON.stringify(response, null, 2))
 
-  const posts = response.results.map(post => {
+  const posts = response.results.map( post => {
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
@@ -149,8 +161,8 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
-  return {
-    props: {
+  return{
+    props:{
       posts,
       page: response.page,
       totalPage: response.total_pages
